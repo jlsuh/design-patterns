d017f43d617ffece0e2e120535f91d974776ff6b
jlsuh <joelleonardosuh@gmail.com>
2022-08-09 17:33:21 -0300

    chore: strategy

--- java/src/main/java/strategy/Ruta.java
+++ java/src/main/java/strategy/Ruta.java
@@ -1,5 +1,6 @@
 package main.java.strategy;
 
+// Strategy también puede ser implementado mediante una clase abstracta
 public interface Ruta {
     // No será sobre diseño siempre y cuando construirRuta() tenga una complejidad considerable.
     // De lo contrario se lo considera sobre diseño.
