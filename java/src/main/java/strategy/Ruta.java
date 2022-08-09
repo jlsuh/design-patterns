3139ff1d05d5893c2dc50aef3a95f0606bd997bb
jlsuh <joelleonardosuh@gmail.com>
2022-08-09 17:21:17 -0300

    chore: strategy

--- java/src/main/java/strategy/Ruta.java
+++ java/src/main/java/strategy/Ruta.java
@@ -1,5 +1,7 @@
 package main.java.strategy;
 
 public interface Ruta {
+    // No será sobre diseño siempre y cuando construirRuta() tenga una complejidad considerable.
+    // De lo contrario se lo considera sobre diseño.
     public void construirRuta();
 }
